f246a6199c63a88df6dde05a326b3f17b34ca8ae
washappbeta <guilhermecapx13@gmail.com>
2024-10-04 20:20:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (email && password) {
+    // Aqui você pode adicionar uma chamada para a API de login
+    window.location.href = "https://eescolaviver.github.io/servicos.html"
+  } else {
+    alert('Por favor, preencha os campos!');
+  }
+});
+
+
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+
+  if (password !== confirmPassword) {
+    alert('As senhas não coincidem!');
+    return;
+  }
+
+  if (name && email && password && confirmPassword) {
+    // Aqui você pode adicionar a chamada para a API de cadastro
+    alert(`Cadastro realizado com sucesso! \nNome: ${name}\nEmail: ${email}`);
+  } else {
+    alert('Por favor, preencha todos os campos!');
+  }
+});
